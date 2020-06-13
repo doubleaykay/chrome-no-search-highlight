c5dc2b4e49524fc7798fb6a0aacaa6eca2bc8a12
doubleaykay <the.a.cars@gmail.com>
2020-06-12 22:10:26 -0400

    test regex fix

--- backgroundscript.js
+++ backgroundscript.js
@@ -1,7 +1,7 @@
 function remove_text_highlight( details ) {
-	if ( details.url.match( /:~:text=*/ ) != null) {
+	if ( details.url.match( /:~:text=[a-zA-Z0-9\-]*/ ) != null) {
 		return {
-			redirectUrl: details.url.replace( /(:~:text=*)/, '' )
+			redirectUrl: details.url.replace( /(:~:text=[a-zA-Z0-9\-]*)/, '' )
 		};
 	}
 }
